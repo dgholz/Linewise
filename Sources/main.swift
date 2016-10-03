6b9bf91f0814efc6a05e7ee9acf8e301dfbc3ae7
Daniel Holz <dgholz@gmail.com>
2016-10-03 00:22:56 -0700

    Delete main.swift
    So that the linux build stops saying, "multiple definition of 'main'"

--- Sources/main.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-for arg in CommandLine.arguments.dropFirst() {
-    if let r = InputStream(fileAtPath: arg) {
-        for line in r.lines() {
-            print("xx>> [\(line)]")
-        }
-    }
-}
